5aad98ac7207088b77ceab8f9af2ec19fb924721
Sergio Cano <li_sergiocm@unca.edu.mx>
2021-03-30 18:56:23 -0600

    se agrego la funcion del evento del scroll hacia abajo

--- js/app.js
+++ js/app.js
@@ -1,18 +1,26 @@
 
 document.addEventListener('DOMContentLoaded', function(){   
+    var evento = document.getElementById('evento');
     window.onscroll = ()=>{
-        function reducciondeHeader() {
-            if (document.body.scrollTop > 50 || document.documentElement.scrollTop > 50) {
-                document.getElementById("derecha").className = "derecha";
-                document.getElementById("abajo").className = "abajo";
-                document.getElementById("logo").className = "pequeno";
-                document.getElementById("minimo").className = 'minimo';
-            } else{
-                document.getElementById("derecha").className = "back";
-                document.getElementById("abajo").className = "regreso";
-                document.getElementById("logo").className = "";
-                document.getElementById("minimo").className = "";
-            }
+        reducciondeHeader();
+    };
+
+    evento.addEventListener('click', function(){     
+        /* document.body.scrollTop = 400; */
+        document.documentElement.scrollTop = 500; 
+    })
+
+    function reducciondeHeader() {
+        if (document.body.scrollTop > 50 || document.documentElement.scrollTop > 50) {
+            document.getElementById("derecha").className = "derecha";
+            document.getElementById("abajo").className = "abajo";
+            document.getElementById("logo").className = "pequeno";
+            document.getElementById("minimo").className = 'minimo';
+        } else{
+            document.getElementById("derecha").className = "back";
+            document.getElementById("abajo").className = "regreso";
+            document.getElementById("logo").className = "";
+            document.getElementById("minimo").className = "";
         }
-    };    
+    }
 });
