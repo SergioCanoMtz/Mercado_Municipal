c29f04ce091f7a2e9869e6c9b632c9cc9c4a4bc5
Sergio Cano <li_sergiocm@unca.edu.mx>
2021-05-06 01:02:00 -0500

    Se logró el efecto suave del scroll

--- js/app.js
+++ js/app.js
@@ -1,8 +1,14 @@
-document.addEventListener('DOMContentLoaded', function(){   
+document.addEventListener('DOMContentLoaded', () => {   
     var evento = document.getElementById('evento');
 
-    evento.addEventListener('click', function(){     
-        /* document.body.scrollTop = 400; */
-        document.documentElement.scrollTop = 500; 
+    evento.addEventListener('click', () => {
+        var count = 1;
+        var proceso = setInterval(() => {
+            document.documentElement.scrollTop += 10; 
+            count++;
+            if( count == 45 ){
+                clearInterval(proceso);
+            }
+        }, 17); 
     })
 });
